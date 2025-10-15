8af2d2a067765879936528c71689e50f92f9dd46
HerbieHolness <48385702+HerbieHolness@users.noreply.github.com>
2025-10-15 11:28:27 +0100

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -2,6 +2,7 @@ let isDarkMode = true;
 let toggleBtn;
 let lightModeIcon, darkModeIcon;
 
+
 function preload() {
   lightModeIcon = loadImage('sun.png');
   darkModeIcon = loadImage('moon.png');
@@ -10,8 +11,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-
-  toggleBtn = createImg(darkModeIcon.canvas.toDataURL(), 'Toggle dark mode');
+  toggleBtn = createImg(lightModeIcon.canvas.toDataURL(), 'Toggle dark mode');
   toggleBtn.id('toggle-btn');
   toggleBtn.size(100, 100);
   toggleBtn.position(20, 20);
@@ -23,38 +23,38 @@ function windowResized() {
 }
 
 function draw() {
-  rectMode(CORNER);
-  colorMode(RGB);
+
   fill(0, 0, 255, 10);
   rect(0, 0, width, height);
-
+  rectMode(CORNER);
+  colorMode(RGB);
   if (isDarkMode) {
     fill(20, 20, 20, 100);
   } else {
     fill(255, 255, 255, 10);
   }
   rect(0, 0, width, height);
-
   colorMode(HSB, 360, 100, 100, 100);
   let t = millis() / 1000;
   let num = map(sin((TWO_PI / 5) * t), -1, 1, 25, 50);
   let hueValue = (t * 60) % 360;
   let x = mouseX || touches[0]?.x || width / 2;
   let y = mouseY || touches[0]?.y || height / 2;
+
   fill(hueValue, 100, 100);
   ellipse(x, y, num);
 }
 
-function toggleMode() {
-  isDarkMode = !isDarkMode;
-
-  document.body.classList.toggle('dark-mode');
-
-  toggleBtn.elt.src = isDarkMode 
-    ? darkModeIcon.canvas.toDataURL()
-    : lightModeIcon.canvas.toDataURL();
-}
-
 function touchMoved() {
   return false;
+}
+function toggleMode() {
+  isDarkMode = !isDarkMode;
+  document.body.classList.toggle('dark-mode');
+  
+  if (isDarkMode) {
+    toggleBtn.elt.src = lightModeIcon.canvas.toDataURL();
+  } else {
+    toggleBtn.elt.src = darkModeIcon.canvas.toDataURL();
+  }
 }
